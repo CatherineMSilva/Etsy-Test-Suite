e9a30b3e0eef23be89ce1a1063c90f270af7547b
Catherine Silva <catherinemichellsilva@gmail.com>
2024-04-02 22:50:18 -0400

    Update Main.java
    Adding some basic information to the main java file so that it can be used as an introduction to the project.

--- Main.java
+++ Main.java
@@ -1,1 +1,11 @@
-
+// Authors: Sahbah Gonzalez and Catherine Silva
+// Date:    April 1st, 2024
+// Class:   Software Testing - CEN 4072
+// Title:   End-to-End Testing of Etsy
+public class Main {
+        // This project consists of all classes necessary to complete an automated end to end suite testing of a web application.
+    // The web application of choice for our project was the E-commerce website Etsy because it can showcase all of the many ways
+    // one can not only buy, but also sell on the website.
+        // In this project we use Selenium commands along with test annotations to create a readable efficient algorithm for testing
+    // a website to completion.
+}
