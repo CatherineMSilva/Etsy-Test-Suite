ead771f52ecb1877ac8708e3d34b5f6ed328785f
sahbahgonzalez <116740133+sahbahgonzalez@users.noreply.github.com>
2024-04-16 11:02:02 -0400

    Update Profile_Edit.java

--- Profile_Edit.java
+++ Profile_Edit.java
@@ -2,107 +2,75 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-public class Profile_Edit {
+public class Profile_Edit extends baseClass{
+    //WebDriver driver = new ChromeDriver();
 
-    @Test(priority = 1) void addProfilePicture() throws InterruptedException{
-        System.setProperty("web driver.chrome.driver", "C:\\Users\\USER\\Downloads\\chromedriver-win32\\chromedriver-win32\\chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
-        driver.get("https://www.etsy.com/");
-        driver.manage().window().maximize();
 
-        WebElement SingInButton = driver.findElement(By.cssSelector("#gnav-header-inner > div.wt-flex-shrink-xs-0 > nav > ul > li:nth-child(1) > button"));
-        SingInButton.click();
-        Thread.sleep(3000);
-        WebElement emailTexbox = driver.findElement(By.xpath("//*[@id=\"join_neu_email_field\"]"));
-        emailTexbox.sendKeys("Velveeta_Cheese@outlook.com");
-        WebElement passTextbox = driver.findElement(By.xpath("//*[@id=\"join_neu_password_field\"]"));
-        passTextbox.sendKeys("Cheese!");
-
-        WebElement logInButton = driver.findElement(By.xpath("//*[@id=\"join-neu-form\"]/div[1]/div/div[7]/div/button"));
-        logInButton.click();
-        Thread.sleep(3000);
-        WebElement Profile_dropDown = driver.findElement(By.cssSelector("#gnav-header-inner > div.wt-flex-shrink-xs-0 > nav > ul > li:nth-child(4) > div > button"));
+    @BeforeMethod
+    public void navigateToHomePage() throws InterruptedException {
+        WebElement Profile_dropDown = driver.findElement(By.cssSelector("#gnav-header-inner > div.wt-flex-shrink-xs-0 > nav > ul > li:nth-child(5) > div > button > span.wt-menu__trigger__label"));
         Profile_dropDown.click();
         Thread.sleep(3000);
-        WebElement ViewYourProf = driver.findElement(By.cssSelector("#gnav-header-inner > div.wt-flex-shrink-xs-0 > nav > ul > li:nth-child(4) > div > div > ul > li:nth-child(1) > a > span > h4"));
+        WebElement ViewYourProf = driver.findElement(By.cssSelector("#gnav-header-inner > div.wt-flex-shrink-xs-0 > nav > ul > li:nth-child(5) > div > div > ul > li:nth-child(1) > a > span > p"));
         ViewYourProf.click();
         Thread.sleep(3000);
+
+    }
+
+    @Test(priority = 1) void addProfilePicture() throws InterruptedException{
+
+        WebElement EditPubProf = driver.findElement(By.cssSelector("#focused_search_large_header > div > div > div.wt-order-xs-2.wt-order-md-1.wt-grid__item-xs-12.wt-grid__item-md-11 > div > div > div > a"));
+        EditPubProf.click();
+        Thread.sleep(3000);
+
+        String filePath = "C:\\Users\\USER\\Downloads\\stuff\\tmp";
+        WebElement Choose_file = driver.findElement(By.cssSelector("#avatar"));
+        Choose_file.sendKeys(filePath);
+    }
+    @Test(priority = 2) void genderRadio() throws InterruptedException{
+
         WebElement Popup = driver.findElement(By.cssSelector("#photo-upload-nudge > span.wt-display-flex-xs > button > span > svg"));
         Popup.click();
         Thread.sleep(3000);
         WebElement EditPubProf = driver.findElement(By.cssSelector("#focused_search_large_header > div > div > div.wt-order-xs-2.wt-order-md-1.wt-grid__item-xs-12.wt-grid__item-md-11 > div > div > div > a"));
         EditPubProf.click();
+
+        WebElement genRadio = driver.findElement(By.xpath("//*[@id=\"male\"]"));
+        genRadio.click();
     }
-    @Test(priority = 2) void searchTest() throws InterruptedException {
-        System.setProperty("web driver.chrome.driver", "C:\\Users\\USER\\Downloads\\chromedriver-win32\\chromedriver-win32\\chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
-        driver.get("https://www.etsy.com/");
-        driver.manage().window().maximize();
+    @Test(priority = 3) void changeCity() throws InterruptedException {
 
-        WebElement SingInButton = driver.findElement(By.cssSelector("#gnav-header-inner > div.wt-flex-shrink-xs-0 > nav > ul > li:nth-child(1) > button"));
-        SingInButton.click();
+        WebElement Popup = driver.findElement(By.cssSelector("#photo-upload-nudge > span.wt-display-flex-xs > button > span > svg"));
+        Popup.click();
         Thread.sleep(3000);
-        WebElement emailTexbox = driver.findElement(By.xpath("//*[@id=\"join_neu_email_field\"]"));
-        emailTexbox.sendKeys("Velveeta_Cheese@outlook.com");
-        WebElement passTextbox = driver.findElement(By.xpath("//*[@id=\"join_neu_password_field\"]"));
-        passTextbox.sendKeys("Cheese!");
+        WebElement EditPubProf = driver.findElement(By.cssSelector("#focused_search_large_header > div > div > div.wt-order-xs-2.wt-order-md-1.wt-grid__item-xs-12.wt-grid__item-md-11 > div > div > div > a"));
+        EditPubProf.click();
 
-        WebElement logInButton = driver.findElement(By.xpath("//*[@id=\"join-neu-form\"]/div[1]/div/div[7]/div/button"));
-        logInButton.click();
-        Thread.sleep(3000);
+        WebElement cityTextbox = driver.findElement(By.xpath("//*[@id=\"city3\"]"));
+        cityTextbox.sendKeys("Fort Myers, Florida, United States Of America");
 
-        WebElement searchBox = driver.findElement(By.xpath("//*[@id=\"global-enhancements-search-query\"]"));
-        searchBox.sendKeys("Wooden Tables");
-        WebElement searchButton = driver.findElement(By.cssSelector("#gnav-search > div > div.wt-input-btn-group.global-enhancements-search-input-btn-group.emphasized_search_bar.emphasized_search_bar_grey_bg.search-bar-container > button.wt-input-btn-group__btn.global-enhancements-search-input-btn-group__btn > span > svg"));
-        searchButton.click();
-        Thread.sleep(3000);
-        WebElement searchBox2 = driver.findElement(By.xpath("//*[@id=\"global-enhancements-search-query\"]"));
-        searchBox2.clear();
-        searchBox2.sendKeys("cool socks");
-        WebElement searchButton2 = driver.findElement(By.cssSelector("#gnav-search > div > div.wt-input-btn-group.global-enhancements-search-input-btn-group.emphasized_search_bar.emphasized_search_bar_grey_bg.search-bar-container > button.wt-input-btn-group__btn.global-enhancements-search-input-btn-group__btn > span > svg"));
-        searchButton2.click();
     }
-    @Test(priority = 3) void filterSearch() throws InterruptedException{
-        System.setProperty("web driver.chrome.driver", "C:\\Users\\USER\\Downloads\\chromedriver-win32\\chromedriver-win32\\chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
-        driver.get("https://www.etsy.com/");
-        driver.manage().window().maximize();
+    @Test(priority = 4) void editBirthday() throws InterruptedException{
 
-        WebElement SingInButton = driver.findElement(By.cssSelector("#gnav-header-inner > div.wt-flex-shrink-xs-0 > nav > ul > li:nth-child(1) > button"));
-        SingInButton.click();
+        WebElement Popup = driver.findElement(By.cssSelector("#photo-upload-nudge > span.wt-display-flex-xs > button > span > svg"));
+        Popup.click();
         Thread.sleep(3000);
-        WebElement emailTexbox = driver.findElement(By.xpath("//*[@id=\"join_neu_email_field\"]"));
-        emailTexbox.sendKeys("Velveeta_Cheese@outlook.com");
-        WebElement passTextbox = driver.findElement(By.xpath("//*[@id=\"join_neu_password_field\"]"));
-        passTextbox.sendKeys("Cheese!");
+        WebElement EditPubProf = driver.findElement(By.cssSelector("#focused_search_large_header > div > div > div.wt-order-xs-2.wt-order-md-1.wt-grid__item-xs-12.wt-grid__item-md-11 > div > div > div > a"));
+        EditPubProf.click();
 
-        WebElement logInButton = driver.findElement(By.xpath("//*[@id=\"join-neu-form\"]/div[1]/div/div[7]/div/button"));
-        logInButton.click();
-        Thread.sleep(3000);
+        WebElement monthDrop = driver.findElement(By.xpath("//*[@id=\"birth-month\"]"));
+        Select dropdown = new Select(monthDrop); //using select class create object dropdown.
+        dropdown.selectByIndex(6); // select 2nd index in dropdown menu.
+        WebElement dayDrop = driver.findElement(By.xpath("//*[@id=\"birth-day\"]"));
+        dropdown = new Select(dayDrop); //using select class create object dropdown.
+        dropdown.selectByIndex(11);
 
-        WebElement searchBox = driver.findElement(By.xpath("//*[@id=\"global-enhancements-search-query\"]"));
-        searchBox.sendKeys("Wooden Tables");
-        WebElement searchButton = driver.findElement(By.cssSelector("#gnav-search > div > div.wt-input-btn-group.global-enhancements-search-input-btn-group.emphasized_search_bar.emphasized_search_bar_grey_bg.search-bar-container > button.wt-input-btn-group__btn.global-enhancements-search-input-btn-group__btn > span > svg"));
-        searchButton.click();
-        Thread.sleep(3000);
-        WebElement filterButton = driver.findElement(By.xpath("//*[@id=\"search-filter-button\"]/span[2]"));
-        filterButton.click();
-        Thread.sleep(3000);
-        WebElement locationRadio = driver.findElement(By.xpath("//*[@id=\"search-filter-form\"]/div/div[1]/div[2]/fieldset/div/div/div[2]/label"));
-        locationRadio.click();
-        //WebElement locationBox = driver.findElement(By.xpath("//*[@id=\"shop-location-input\"]"));
-        //locationBox.sendKeys("florida");
-        Thread.sleep(3000);
-        WebElement saleCheckBox = driver.findElement(By.xpath("//*[@id=\"search-filter-form\"]/div/div[1]/div[1]/fieldset/div/div/div[2]/div/label"));
-        saleCheckBox.click();
-        Thread.sleep(3000);
-        WebElement resetButton = driver.findElement(By.xpath("//*[@id=\"search-filter-form\"]/div/div[1]/div[2]/fieldset/div/div/div[2]/div/label"));
-        resetButton.click();
-        WebElement exitButton = driver.findElement(By.xpath("//*[@id=\"search-filters-overlay\"]/div/button/span/svg"));
-        exitButton.click();
-
-
+        WebElement saveChanges = driver.findElement(By.xpath("//*[@id=\"content\"]/div/form/div[2]/input"));
+        saveChanges.click();
     }
 }
