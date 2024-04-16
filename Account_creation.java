5acffc540937acc21859a4be144a8c6a02bcd69e
sahbahgonzalez <116740133+sahbahgonzalez@users.noreply.github.com>
2024-04-16 11:00:16 -0400

    Update Account_creation.java

--- Account_creation.java
+++ Account_creation.java
@@ -1,32 +1,43 @@
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.support.ui.Select;
-
 import java.util.ArrayList;
-import java.util.List;
 
 public class Account_creation {
-        @Test(priority = 1)
-        void InValid_Cred() throws InterruptedException {
-                System.setProperty("web driver.chrome.driver", "C:\\Users\\USER\\Downloads\\chromedriver-win32\\chromedriver-win32\\chromedriver.exe");
-                WebDriver driver = new ChromeDriver();
+       // public WebDriver driver;
+       WebDriver driver = new ChromeDriver();
+        @BeforeClass
+        public void setUp() throws InterruptedException {
+
+                // Initialize the WebDriver and open the browser once before any tests start
+                System.setProperty("webdriver.chrome.driver", "C:\\Users\\USER\\Downloads\\chromedriver-win32\\chromedriver-win32\\chromedriver.exe");
+
+                driver.manage().window().maximize();
+                // Navigate to the website at the beginning of each test
                 driver.get("https://www.etsy.com/");
                 driver.manage().window().maximize();
-
                 WebElement SingInButton = driver.findElement(By.cssSelector("#gnav-header-inner > div.wt-flex-shrink-xs-0 > nav > ul > li:nth-child(1) > button"));
                 SingInButton.click();
+                Thread.sleep(8000);
+        }
+
+        @BeforeMethod
+        public void navigateToHomePage() throws InterruptedException {
+
 
                 Thread.sleep(3000);
                 WebElement RegistrationButton = driver.findElement(By.cssSelector("#join-neu-form > div.wt-grid.wt-grid--block > div > div:nth-child(1) > div > button"));
                 RegistrationButton.click();
 
+        }
+
+        @Test(priority = 1)
+        void InValid_Cred() throws InterruptedException {
                 Thread.sleep(3000);
                 WebElement EmailTextBox = driver.findElement(By.xpath("//*[@id=\"join_neu_email_field\"]"));
                 EmailTextBox.sendKeys("Hello.@gmail.com");
@@ -41,21 +52,9 @@ public class Account_creation {
 
                 WebElement subButton = driver.findElement(By.cssSelector("#join-neu-form > div.wt-grid.wt-grid--block > div > div:nth-child(10) > div > button"));
                 subButton.click();
-                driver.close();
+                Thread.sleep(3000);
         }
         @Test(priority = 2) void Valid_Cred() throws InterruptedException {
-                System.setProperty("web driver.chrome.driver", "C:\\Users\\USER\\Downloads\\chromedriver-win32\\chromedriver-win32\\chromedriver.exe");
-                WebDriver driver = new ChromeDriver();
-                driver.get("https://www.etsy.com/");
-                driver.manage().window().maximize();
-
-                WebElement SingInButton = driver.findElement(By.cssSelector("#gnav-header-inner > div.wt-flex-shrink-xs-0 > nav > ul > li:nth-child(1) > button"));
-                SingInButton.click();
-
-                Thread.sleep(3000);
-                WebElement RegistrationButton = driver.findElement(By.cssSelector("#join-neu-form > div.wt-grid.wt-grid--block > div > div:nth-child(1) > div > button"));
-                RegistrationButton.click();
-
                 Thread.sleep(3000);
                 WebElement EmailTextBox = driver.findElement(By.xpath("//*[@id=\"join_neu_email_field\"]"));
                 EmailTextBox.sendKeys("Velveeta_Cheese@outlook.com");
@@ -76,11 +75,6 @@ public class Account_creation {
                 exitButton.click();
         }
         @Test(priority = 3) void Verify_email() throws InterruptedException{
-                System.setProperty("web driver.chrome.driver", "C:\\Users\\USER\\Downloads\\chromedriver-win32\\chromedriver-win32\\chromedriver.exe");
-                WebDriver driver = new ChromeDriver();
-                driver.get("https://www.etsy.com/");
-                driver.manage().window().maximize();
-                //driver.manage().window().maximize();
                 String originalTab = driver.getWindowHandle();
                 ((JavascriptExecutor)driver).executeScript("window.open()");
                 ArrayList<String> tabs = new ArrayList<>(driver.getWindowHandles());
@@ -88,12 +82,13 @@ public class Account_creation {
                 driver.get("https://outlook.live.com/mail/0/");
 
                 Thread.sleep(5000);
-                WebElement signInButton = driver.findElement(By.xpath("//*[@id=\"feature-oc387eee\"]/div/div/div[1]/div/div[3]/div[1]/a"));
+                WebElement signInButton = driver.findElement(By.xpath("//*[@id=\"action-oc5b26\"]/span"));
                 signInButton.click();
 
                 Thread.sleep(3000);
                 ArrayList<String> tab = new ArrayList<>(driver.getWindowHandles());
                 driver.switchTo().window(tab.get(2));
+                Thread.sleep(3000);
                 WebElement signTexbox = driver.findElement(By.xpath("//*[@id=\"i0116\"]"));
                 signTexbox.sendKeys("Velveeta_Cheese@outlook.com");
                 WebElement nextButton = driver.findElement(By.xpath("//*[@id=\"idSIButton9\"]"));
@@ -110,22 +105,13 @@ public class Account_creation {
                 NoButton.click();
 
                 Thread.sleep(5000);
-                WebElement emailView = driver.findElement(By.xpath("//*[@id=\"AQAAAAAAAQABAAAAAGBdmQAAAAA=\"]"));
+                WebElement emailView = driver.findElement(By.xpath("//*[@id=\"AQAAAAAAAQABAAAAAGBdmQAAAAA=\"]/div/div/div[2]/div[2]/div[2]/div/span"));
                 emailView.click();
-                //WebElement confirm = driver.findElement(By.xpath("//*[@id=\"UniqueMessageBody\"]/div/div/div/div/table/tbody/tr[3]/th/table/tbody/tr/td/table/tbody/tr/td/table[1]/tbody/tr/td/div/a"));
-                //confirm.click();
-                //driver.switchTo().window(originalTab);
-
+                Thread.sleep(5000);
+                WebElement confirm = driver.findElement(By.cssSelector("#UniqueMessageBody > div > div > div > div > table > tbody > tr:nth-child(3) > th > table > tbody > tr > td > table > tbody > tr > td > table:nth-child(1) > tbody > tr > td > div > a"));
+                confirm.click();
+                Thread.sleep(5000);
+                driver.switchTo().window(originalTab);
         }
-
-
-
-
-
-
-
-
-
-
 }
 
